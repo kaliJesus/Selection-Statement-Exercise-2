e91b901857b179170669fb285d875e59b3baae94
Kalkidan Shibeshi <kkalitemesgen@gmail.com>
2023-02-07 19:17:21 -0500

    new commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Net.Http.Headers;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            string FavSubject = Console.ReadLine();
+
+            switch (FavSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is the best");
+                break;
+                case "biology":
+                    Console.WriteLine("Biology is the best");
+                break;
+                case "physics":
+                    Console.WriteLine("physics rules");
+                break;
+                case "history":
+                    Console.WriteLine("Interesting");
+                break;
+                case "music":
+                    Console.WriteLine("Nice!");
+                    break;
+                default:
+                    Console.WriteLine("Please enter a subject");
+                break;
+
+
+            }
+
         }
     }
 }
